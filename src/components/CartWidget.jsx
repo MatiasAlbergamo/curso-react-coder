7dcf0ae0c3d30a7b2dab5da760b135aad7f17218
MatiasAlbergamo <albergamomatias@gmail.com>
2023-08-24 18:21:40 -0300

    commit primera preentrega

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,20 @@
+import { Box, Divider, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const CartWidget = () => {
+return (
+    <div>
+        <Flex>
+            <Box>
+                <span className="material-symbols-outlined">shopping_cart</span>
+            </Box>
+            <Divider/>
+            <Box>
+                <p>6</p>
+            </Box>
+        </Flex>
+    </div>
+)
+}
+
+export default CartWidget
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Flex, Box, Spacer,
+
+} from '@chakra-ui/react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+return (
+    <div>
+
+<Flex>
+    <Box p='4'>
+        <h3>Albero Bianco</h3>
+    </Box>
+    <Spacer />
+    <Box p='4'>
+        <Menu>
+            <MenuButton>
+                Categories
+            </MenuButton>
+            <MenuList>
+                <MenuItem>Category A</MenuItem>
+                <MenuItem>Category B</MenuItem>
+                <MenuItem>Category C</MenuItem>
+            </MenuList>
+        </Menu>
+    </Box>
+    <Spacer />
+    <Box p='4'>
+        <CartWidget/>
+    </Box>
+</Flex>
+
+
+</div>
+)
+}
+
+export default NavBar
